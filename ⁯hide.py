57f0043b1725b821f60471d64f249474cf517fb5
Bryan <bryanarmondoayala@gmail.com>
2021-11-15 08:46:48 -0600

    Hey there

---
+++ ⁯hide.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 19 08:11:31 2021
+
+@author: XLHS
+"""    
+numberlist = [20,34,92,18,103]
+big = 0
+for item in range(5):
+    print(numberlist[item])
+
+
+
+print(maxim([2,5,4,6,7,3,5]))
+
+   
+import winsound
+
+winsound.Beep(300, 300)
